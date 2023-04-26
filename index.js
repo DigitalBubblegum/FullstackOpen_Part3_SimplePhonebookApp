9e43e86d64dd96720d96712585c76c8f57eb7679
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-26 09:54:38 +0530

    -added functionalities for get and post

--- index.js
+++ index.js
@@ -1,100 +1,106 @@
-console.log('Starting the phonebook api server');
-const express = require('express')
-const app = express()
+const express = require("express");
+var morgan = require("morgan");
+const app = express();
+
+morgan.token("mes", function getMes(request) {
+  return JSON.stringify(request.body);
+});
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length] :mes")
+);
+app.use(express.json());
+// app.use(morgan)
+
 let persons = [
   {
     id: 1,
     name: "Arto Hellas",
-    phone: "111111",
+    number: "040-123456",
   },
   {
     id: 2,
-    name: "Emma Johnson",
-    phone: "222222",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    name: "Michael Chen",
-    phone: "333333",
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
   {
     id: 4,
-    name: "Sara Lee",
-    phone: "444444",
-  },
-  {
-    id: 5,
-    name: "David Kim",
-    phone: "555555",
-  },
-  {
-    id: 6,
-    name: "Sophie Martinez",
-    phone: "666666",
-  },
-  {
-    id: 7,
-    name: "Jackie Wong",
-    phone: "777777",
-  },
-  {
-    id: 8,
-    name: "Rahul Gupta",
-    phone: "888888",
-  },
-  {
-    id: 9,
-    name: "Emily Thompson",
-    phone: "999999",
-  },
-  {
-    id: 10,
-    name: "Nate Johnson",
-    phone: "101010",
-  },
-  {
-    id: 11,
-    name: "Lila Rodriguez",
-    phone: "111111",
-  },
-  {
-    id: 12,
-    name: "Mia Davis",
-    phone: "121212",
-  },
-  {
-    id: 13,
-    name: "Ethan Brown",
-    phone: "131313",
-  },
-  {
-    id: 14,
-    name: "Samantha Patel",
-    phone: "141414",
-  },
-  {
-    id: 15,
-    name: "Maxwell Wilson",
-    phone: "00000",
-  },
-  {
-    id: 16,
-    name: "Ava Garcia",
-    phone: "161616",
-  },
-  {
-    name: "Michael Scott",
-    phone: "8977567567",
-    id: 17,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
-app.get("/", (request, response) => {
-  response.send("<h1>Hello Welcome to phonebook API please navigate to /api/persons for the phonenumbers</h1>");
+
+//functions
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+app.get("/info", (request, response) => {
+  const length = persons.length;
+  const currDate = new Date();
+  console.log(currDate);
+  response.send(
+    `<p>phonebook has info for ${length} people</p><br><p>${currDate}</p>`
+  );
 });
-app.get('/api/persons',(request,response)=>{
-    response.json(persons)
-})
-const PORT = 3001
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`);
-})
+//view all
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+//view by id
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).send("<h1>404 not found</h1>").end();
+  }
+});
+//delete by id
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons.filter((person) => person.id != id);
+  response.status(404).end();
+  console.log("deleted");
+});
+//add a number
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  if (persons.map((person) => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  } else {
+    const person = {
+      id: getRandomIntInclusive(0, 1000000000000),
+      name: body.name,
+      number: body.number,
+    };
+
+    persons = persons.concat(person);
+    response.json(person);
+  }
+});
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
+// console.log(JSON.stringify(persons));
